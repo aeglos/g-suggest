76aa38f7baefd59fffc3f1c93d6d8d7a753f912c
Han-Pei Lin <aeglos.tw@gmail.com>
2013-04-25 23:58:16 +0800

    Set search term to first command line argument
    Signed-off-by: Han-Pei Lin <aeglos.tw@gmail.com>

--- g-suggest.py
+++ g-suggest.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*
+# -*- coding: utf-8 -*-
+
 import urllib2
+import sys
 
 GOOGLE_SUGGEST = "http://suggestqueries.google.com/complete/search?output=toolbar&hl=en&oe=utf8&q="
+EXAMPLE_TERM = "不可以"
 
-mysuggest = GOOGLE_SUGGEST + "RTFM"
+def getxml(search_term):
+    mysuggest = GOOGLE_SUGGEST + urllib2.quote(search_term)
 
-response = urllib2.urlopen(mysuggest)
-html = response.read()
-print html
+    response = urllib2.urlopen(mysuggest)
+    html = response.read()
+    return html
+
+def main():
+    if  len(sys.argv) != 2:
+        print "Need one search term!"
+        xml = getxml(EXAMPLE_TERM)
+    else:
+        print "Get Google suggest queries of " + sys.argv[1]
+        xml = getxml(sys.argv[1])
+
+    print xml
+
+if __name__ == '__main__':
+    main()
+
